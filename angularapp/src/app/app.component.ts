5ea5e50266604561b54e45571cd0fb456f391686
Iamneo <techadmin@iamneo.ai>
2023-08-14 14:01:29 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -6,16 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp';
-  opt1:number;
-  opt2:number;
-  amt:number;
-  res:number;
-  result:string;
-  exchange(a:number,b:number,amt:number){
-    if(a!=null && b!=null){
-      this.result=Math.round(b/a*amt).toFixed(2);
-      return this.result;
+  fromCurrency: string = 'USD';
+  toCurrency: string = 'USD';
+  amount: number = 0;
+  conversionResult: string = '';
+
+  convert() {
+    const exchangeRates = {
+      USD: 1.126735,
+      GBP: 0.876893,
+      INR: 79.677056
+    };
+
+    const fromRate = exchangeRates[this.fromCurrency];
+    const toRate = exchangeRates[this.toCurrency];
+
+    if (fromRate && toRate) {
+      const convertedAmount = (this.amount / fromRate) * toRate;
+      this.conversionResult = `Converted Amount: ${convertedAmount.toFixed(2)} ${this.toCurrency}`;
+    } else {
+      this.conversionResult = 'Invalid currency selection.';
     }
   }
 }
